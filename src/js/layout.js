f8925ec924431d92c0526f0057fe7b57b2f659fb
bertomeu <josecarlosbertomeu@gmail.com>
2024-08-07 15:56:13 +0000

    modified layout.js

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { Context } from "./store/appContext";
 import injectContext from "./store/appContext";
+import { CharacterDetail } from "./component/CharacterDetail";
+import { PlanetDetail } from "./component/PlanetDetail";
+import { VehicleDetail } from "./component/VehicleDetail";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import NavbarTop from "./component/navbarTop";
+import StarField from "./component/StartField";
+
+
+
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+    const { store } = useContext(Context);
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
+
+
+
+    return (
+        <div>
+
+            <StarField />
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <NavbarTop />
+                    <Navbar favorites={store.favorites} />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/character/:id" element={<CharacterDetail />} />
+                        <Route path="/vehicle/:id" element={<VehicleDetail />} />
+                        <Route path="/planet/:id" element={<PlanetDetail />} />
+                    </Routes>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+            
+        </div>
+    );
 };
 
 export default injectContext(Layout);
